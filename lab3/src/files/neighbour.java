91b3653beba0bc7bf9419198a7fd7a3860b6f30b
Kors <Kors-iii@qip.ru>
2012-04-21 20:14:03 +0400

    new file:   .classpath 	new file:   .project 	new file:   bin/files/field_of_berries.class 	new file:   bin/files/neighbour.class 	new file:   bin/main.class 	new file:   src/files/field_of_berries.java 	new file:   src/files/neighbour.java 	new file:   src/main.java

---
+++ lab3/src/files/neighbour.java
@@ -0,0 +1,81 @@
+package files;
+
+import files.*;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.Random;
+
+
+public class neighbour implements Runnable {
+
+	Random rand = new Random();
+	//private ReadWriteLock _flag;
+	//private Lock _flag;
+	private int _neighbourNumber = 0 ;
+	private field_of_berries _field;
+	private neighbour _myNeighbour;
+	public boolean _flag = false;
+	private int _countOfGoingToField = 0 ;
+	private int _countOfBerries = 0 ;
+
+	private int _tmp = 0 ;
+
+
+	public neighbour(int neighbourNumber , field_of_berries field) {
+		this._neighbourNumber = neighbourNumber;
+		this._field = field ;
+		//this._flag.lock();
+	}
+
+	public void setNeighbour(neighbour myNeighbour){
+		this._myNeighbour = myNeighbour;
+	}
+
+	public void run() {
+		System.out.println("\nNeighbour "+ _neighbourNumber + " START");
+		while (true){
+			System.out.print("\nсосед " +_neighbourNumber + " собираетс€ набрать €год");
+
+			if ( tryGoToField() ){
+				_tmp = _field.get_berries(_neighbourNumber);
+				System.out.print("\nсосед " + _neighbourNumber + 
+						" вернулс€ к себе, принес€ " + 
+						 _tmp + "кг €год. =)" +
+						"\n и опустил флаг.");
+				_countOfGoingToField += 1 ;
+				_countOfBerries += _tmp;
+				this._flag = false; 
+			}else{
+				System.out.print("\nсосед " +_neighbourNumber + " увидел флаг и ждет =(");
+			}
+
+			try {
+				Thread.sleep(100 + rand.nextInt(2000));
+			} catch (InterruptedException ex) {
+			}
+
+		}
+
+	}
+
+	private boolean tryGoToField(){
+		_flag = true;
+		if ( ! _myNeighbour._flag ){
+			return true;
+		}
+		_flag = false;
+		return false;
+	}
+
+
+
+	public int get_countOfBerries() {
+		return _countOfBerries;
+	}
+
+	public int get_countOfGoingToField() {
+		return _countOfGoingToField;
+	}
+
+}
